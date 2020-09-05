eb7772c739cfa6760612918a871f4ad64ac297b0
jgchenu <jgchenu@foxmail.com>
2020-09-05 18:09:37 +0800

    init: jest test

--- jest-test/jest-app/src/App.test.js
+++ jest-test/jest-app/src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   const result = render(<App />);
+//   const linkElement = result.getByText(/hello world/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it("render without crashing", () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.find(".App"));
 });
