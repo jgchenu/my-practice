119731dfe6774f8bd8a1cfcf84110f0c6a6d09e7
jgchenu <jgchenu@foxmail.com>
2022-02-07 22:52:32 +0800

    feat: add test tree shaking demo

--- hook-demo/src/index.js
+++ hook-demo/src/index.js
@@ -1,1 +1,11 @@
 import "./reducer";
+
+// import React from "react";
+// import { Button } from "antd";
+// import { Button } from "sun-react-ui";
+
+// function App() {
+//   return <Button>button</Button>;
+// }
+
+// export default App;
