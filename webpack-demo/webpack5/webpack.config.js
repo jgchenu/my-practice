94b640d98ec032f3d14ea7e3f1ce38e192af9fdd
jgchenu <jgchenu@foxmail.com>
2022-07-20 22:44:32 +0800

    feat: add style demo

--- webpack-demo/webpack5/webpack.config.js
+++ webpack-demo/webpack5/webpack.config.js
@@ -2,17 +2,12 @@ const path = require("path");
 module.exports = {
   entry: "../src/hash.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+  // mode: "development",
   mode: "production",
-  // cache: {
-  //   type: "filesystem",
-  //   // 可选配置
-  //   buildDependencies: {
-  //     config: [__filename], // 当构建依赖的config文件（通过 require 依赖）内容发生变化时，缓存失效
-  //   },
-  // },
+
   module: {
     rules: [
       {
@@ -23,6 +18,17 @@ module.exports = {
             options: {
               presets: ["@babel/preset-env"],
             },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
           },
         ],
       },
