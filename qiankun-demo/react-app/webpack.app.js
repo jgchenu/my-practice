f17431466af0eb944dc2eabcdedfcb8c57c554f2
光光 <jgchenu@foxmail.com>
2022-10-19 14:54:04 +0800

    Update webpack.app.js

--- qiankun-demo/react-app/webpack.app.js
+++ qiankun-demo/react-app/webpack.app.js
@@ -1,6 +1,8 @@
 const name = require('./package.json').name;
 const path = require('path');
 const { merge } = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const baseConfig = require('./webpack.config');
 const ROOT_PATH = path.resolve(__dirname, '.');
 const __DEV__ = process.env.NODE_ENV === 'development';
@@ -19,6 +21,11 @@ const config = merge(baseConfig, {
       'Access-Control-Allow-Origin': '*',
     },
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: path.resolve(ROOT_PATH, './static/config.json') }],
+    }),
+  ],
 });
 
 module.exports = config;
